3175aecddd16400db9ac87948e61ee2be4ffac6a
Mike Chaliy <mike@chaliy.name>
2011-04-18 17:41:36 +0300

    Added support for connection strings and migration config

--- PsMigrator/PsMigrator.psm1
+++ PsMigrator/PsMigrator.psm1
@@ -2,27 +2,18 @@ function Start-Migration {
 [CmdletBinding()]
 Param(
     
-)
-	function StoreState($state){
-		$buffer = New-Object Text.StringBuilder
-        foreach($key in $state.Keys){			
-            $buffer.AppendLine( $key + "=" + $state[$key] ) | Out-Null  
-        }
-		Set-Content ".PsMigrator" ( $buffer.ToString() )	
-	}
-	
-	function LoadState(){
-		if (Test-Path .PsMigrator) {       
-			Write-Verbose "Load migration state from .PsMigrator file"
-			$psMigratorFullPath = Resolve-Path .PsMigrator
-			return ConvertFrom-StringData ([IO.File]::ReadAllText($psMigratorFullPath))    
-		}	
-		return @{}
-	}
-   	
+)	
+	function ExecuteSqlScript($path){			
+		Import-Module PsSql
+		if ( -not ($state.ConnectionString ) ){
+			Invoke-Sql $_.FullName
+		} else {
+			Invoke-Sql $_.FullName -ConnectionString ( $state.ConnectionString )
+		}
+	}   	
 		
 	# Actual execution
-    $state = LoadState
+    $state = Load-MigrationState
 	    
     $runNextMigration = ( $state.LastMigration -eq $null )
     Get-ChildItem *.ps1 | where-object {$_.Extension -match ".ps1|.sql" } | sort Name | %{
@@ -45,7 +36,7 @@ Param(
         }
     }
     
-    StoreState $state
+    Store-MigrationState $state
     
 <#
 .Synopsis
@@ -60,6 +51,50 @@ Param(
 
 #>
 }
+function Update-MigrationConfig {
+[CmdletBinding()]
+Param(    
+	[String]$ConnectionString
+)
+	$state = Load-MigrationState
+	
+	if ($ConnectionString){
+		$state.ConnectionString = $ConnectionString
+	}
+	
+	Store-MigrationState($state)
+<#
+.Synopsis
+    Setup local migraiton configuration
+.Description
+.Parameter ConnectionString
+    Optional paramter for connection string that command will use to excute script, by default connection string is to local default server and windows authentication.
+.Example
+    Update-MigrationConfig -ConnectionString "Server = FOO;Integrated Security=True"
 
+    Description
+    -----------
+    Setup migration to use given connection string
+
+#>
+}
+
+function Store-MigrationState($state){
+	$buffer = New-Object Text.StringBuilder
+    foreach($key in $state.Keys){			
+        $buffer.AppendLine( $key + "=" + $state[$key] ) | Out-Null  
+    }
+	Set-Content ".PsMigrator" ( $buffer.ToString() )	
+}
+
+function Load-MigrationState(){
+	if (Test-Path .PsMigrator) {       
+		Write-Verbose "Load migration state from .PsMigrator file"
+		$psMigratorFullPath = Resolve-Path .PsMigrator
+		return ConvertFrom-StringData ([IO.File]::ReadAllText($psMigratorFullPath))    
+	}	
+	return @{}
+}
 
 Export-ModuleMember Start-Migration
+Export-ModuleMember Update-MigrationConfig
